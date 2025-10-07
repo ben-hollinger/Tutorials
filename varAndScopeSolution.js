1aebb7cd7489479913a88b91a5fb9fd550d8e6b2
ben-hollinger <bhollin4@uwo.ca>
2025-10-06 22:27:57 -0400

    Update varAndScopeSolution.js

--- varAndScopeSolution.js
+++ varAndScopeSolution.js
@@ -17,7 +17,7 @@ function greetingFunction(saySomethingElse) {
 
   if (saySomethingElse) {
     // Block scope
-    let message = "hi!\nhello!"; //set to a string
+    let message = "hello!"; //set to a string
     console.log(message); 
   }
 }
@@ -32,4 +32,5 @@ if (true) {
 }
 
 //DO NOT change this line
+
 console.log(message);  //should return "goodbye!"
